80c7504d4b8b09fa75fd3af3b45afa5baae23346
MunDo12138 <66548936+MunDo12138@users.noreply.github.com>
2023-05-22 13:27:28 +0800

    Update footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -16,7 +16,7 @@ const footer = () => {
       <div className='gpt3__footer-links'>
         <div className='gpt3__footer-links_logo'>
           <img src={gpt3Logo} alt='logo' />
-          <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+          <p>Tommy Li<br /> All Rights Reserved</p>
         </div>
         <div className='gpt3__footer-links_div'>
           <h4>Links</h4>
@@ -33,14 +33,13 @@ const footer = () => {
         </div>
         <div className="gpt3__footer-links_div">
           <h4>Get in touch</h4>
-          <p>Crechterwoord K12 182 DK Alknjkcb</p>
-          <p>085-132567</p>
-          <p>info@payme.net</p>
+          <p>(+852) 9049-8811)</p>
+          <p>lihungleong623@gmail.com</p>
         </div>
       </div>
 
       <div className="gpt3__footer-copyright">
-        <p>@2021 GPT-3. All rights reserved.</p>
+        <p>@2023 Tommy Li All rights reserved.</p>
       </div>
     </div>
   )
